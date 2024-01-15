94327a45faf3c8b5eecd4a44111721a6954add53
Felipe Vianna <felipe_vianna@mckinsey.com>
2024-01-15 11:35:00 +0100

    finish API

--- src/app.py
+++ src/app.py
@@ -12,8 +12,8 @@ logger.getLogger().setLevel(logger.INFO)
 
 app = FastAPI()
 
-model = Model()
 config = yaml.safe_load(open("/home/app/config/config.yaml", "r"))
+model = Model()
 
 
 def cosine_similarity(u, v):
@@ -24,26 +24,28 @@ def cosine_similarity(u, v):
 async def startup_event():
     if config['download_model']['filename'].removesuffix('.gz') not in \
             os.listdir(config['download_model']['model_path']):
-        logger.info("downloading model...")
+        logger.info("downloading model...\n")
 
         model.fetch_model_from_drive(config['download_model']["url"], config[
             'download_model']["filename"], config['download_model']["model_path"])
     else:
-        logger.info("model already downloaded...")
+        logger.info("model already downloaded...\n")
 
     # load model
     wv = KeyedVectors.load_word2vec_format(
         f"{config['download_model']['model_path']}/"
-                                           f"{config['download_model']['filename'].removesuffix('.gz')}", binary=True, limit = 1000000)
-    model.wv = wv
-    logger.info("model loaded to memory..")
+                                           f"{config['download_model']['filename'].removesuffix('.gz')}", binary=True, limit=1000000)
+    model.load_model(wv)
+    logger.info("model loaded to memory..\n")
 
     # load phrases and tokenize
-    model.phrases = pd.read_csv(config['data']['path'], encoding='latin')
-    model.phrases_dict = {index: row['Phrases'] for index, row in
-                          model.phrases.iterrows()}
+    model.load_phrases(config)
     model.tokenize_phrases()
-    logger.info("data loaded and tokenized..")
+
+    logger.info("data loaded and tokenized...\n")
+    logger.info("embedding data...\n")
+    model.embed_all()
+    logger.info("embedding completed...\n")
 
 
 @app.get("/model")
@@ -53,10 +55,44 @@ async def model_available():
     else:
         return {"message": "model is not loaded"}
 
+
 @app.get("/phrases")
 async def phrases():
     return model.phrases_dict
 
+
 @app.get("/tokenized")
-async def phrases():
+async def tokens():
     return model.tokenized
+
+
+@app.get("/batch_phrases_similarity")
+async def phrases_similarity():
+    sim_df = pd.DataFrame(
+        np.diag(np.diag(np.ones(shape=(len(model.phrases), len(model.phrases))))))
+
+    for c in sim_df.columns:
+        for r in range(0, c + 1):
+            similarity = cosine_similarity(model.phrases_embeddings[c],
+                                           model.phrases_embeddings[r])
+            sim_df[c][r] = similarity
+            sim_df[r][c] = similarity # matrix is simetric on the diagonal
+
+    return sim_df
+
+
+@app.get("/most_similar")
+async def most_similar(sentence):
+    max_similarity = 0
+    max_id = None
+    tokenized_sentence = model.remove_stop_words(sentence)
+    embedded_sentence = model.embed_sentence(tokenized_sentence)
+
+    for id in model.phrases_embeddings:
+        similarity = cosine_similarity(embedded_sentence, model.phrases_embeddings[id])
+        if similarity > max_similarity:
+            max_similarity = similarity
+            max_id = id
+
+    return {"sentence_similar:": model.phrases_dict[max_id],
+            "similarity:": max_similarity}
--- src/model.py
+++ src/model.py
@@ -2,6 +2,11 @@ import os
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import numpy as np
+import pandas as pd
+import logging as logger
+logger.getLogger().setLevel(logger.INFO)
+
 nltk.download('punkt')
 nltk.download('stopwords')
 
@@ -20,6 +25,7 @@ class Model:
         self.phrases = None
         self.phrases_dict = {}
         self.tokenized = {}
+        self.phrases_embeddings = {}
 
     @staticmethod
     def fetch_model_from_drive(self, url, filename, model_path) -> None:
@@ -32,10 +38,36 @@ class Model:
         filtered_sentence = [w for w in word_tokens if not w.lower() in self.stopwords]
         return filtered_sentence
 
+    def load_phrases(self, config):
+        self.phrases = pd.read_csv(config['data']['path'],
+                                    encoding='latin').drop_duplicates().reset_index()
+        self.phrases_dict = {index: row['Phrases'] for index, row in
+                              self.phrases.iterrows()}
+
+    def load_model(self, wv):
+        self.wv = wv
+
     def tokenize_phrases(self):
         for sentence_id in self.phrases_dict:
             tokens = self.remove_stop_words(self.phrases_dict[sentence_id])
             self.tokenized[sentence_id] = tokens
 
-    def embed_sentence(self, sentence):
-        pass
+    def embed_sentence(self, tk_sentence):
+        embedding = np.zeros(300)
+
+        for token in tk_sentence:
+            if token in self.wv.key_to_index:
+                emb_token = self.wv.get_vector(token)
+            else:
+                logger.warning(f"{token} not in vocabulary")
+                emb_token = np.zeros(300)
+
+            embedding += emb_token # sum all embeddings in sentence
+
+        norm_embed = embedding / np.linalg.norm(embedding)
+
+        return norm_embed
+
+    def embed_all(self):
+        for tokenized_id in self.tokenized:
+            self.phrases_embeddings[tokenized_id] = self.embed_sentence(self.tokenized[tokenized_id])
